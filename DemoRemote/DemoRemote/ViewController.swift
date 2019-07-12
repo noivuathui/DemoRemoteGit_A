499b505c81707201db0f1f229f2c8a03bbdb1f52
Nguyễn Đức Tài <tnguyen12@digitalaviationservices.com>
2019-07-12 16:21:26 +0700

    ok

---
+++ DemoRemote/DemoRemote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoRemote
+//
+//  Created by @_@ ndt on 7/12/19.
+//  Copyright © 2019 Nguyễn Đức Tài. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
